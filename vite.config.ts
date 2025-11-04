134b52c18d10f294ba7a815456b559b72b024333
Faraj, Diyar <Diyar.Faraj@emerson.com>
2025-11-04 22:31:08 +0100

    add new bg

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    minify: 'terser',
+    sourcemap: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  server: {
+    port: 3080,
+    open: true,
+  },
+});
